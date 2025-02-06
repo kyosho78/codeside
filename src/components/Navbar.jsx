a64cbfcfe728f1d4b135987706ba41fe531eae82
Valter Backström <dylandante@gmail.com>
2025-02-06 19:53:23 +0200

    added mobile view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,62 +1,83 @@
 import { Link } from "react-router-dom";
-import { FaUniversity } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
-  return (
-    <nav className="fixed w-full flex justify-between items-center p-4 bg-black bg-opacity-90">
-      {/* University Icon as Home */}
-      <Link to="/" className="relative p-2 pl-16">
-        <img src="/coding.svg" alt="Logo" className="w-10 h-10" />
-      </Link>
+  const [isOpen, setIsOpen] = useState(false);
 
-      {/* Navigation Links */}
-      <div className="hidden md:flex space-x-6">
-        <Link
-          to="/about"
-          className="relative !text-[#56afe6] hover:!text-blue-600 after:block after:absolute after:left-1/2 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-600 hover:after:w-full hover:after:left-0"
-        >
-          Ohjelmointi
+  return (
+    <nav className="fixed w-full bg-black bg-opacity-90 p-4">
+      <div className="max-w-7xl mx-auto flex items-center justify-between px-4">
+        {/* Left Section: Logo */}
+        <Link to="/" className="p-2 flex-shrink-0">
+          <img src="/coding.svg" alt="Logo" className="w-10 h-10" />
         </Link>
-        <Link
-          to="/services"
-          className="relative !text-[#56afe6] hover:!text-blue-600 after:block after:absolute after:left-1/2 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-600 hover:after:w-full hover:after:left-0"
+
+        {/* Mobile Menu Button */}
+        <button
+          className="md:hidden text-white text-3xl"
+          onClick={() => setIsOpen(!isOpen)}
         >
-          Alustat
-        </Link>
-        <Link
-          to="/contact"
-          className="relative !text-[#56afe6] hover:!text-blue-600 after:block after:absolute after:left-1/2 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-600 hover:after:w-full hover:after:left-0"
-        >
-          Forum
-        </Link>
-        <Link
-          to="/contact"
-          className="relative !text-[#56afe6] hover:!text-blue-600 after:block after:absolute after:left-1/2 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-600 hover:after:w-full hover:after:left-0"
-        >
-          Sanakirja
-        </Link>
-        <Link
-          to="/contact"
-          className="relative !text-[#56afe6] hover:!text-blue-600 after:block after:absolute after:left-1/2 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-600 hover:after:w-full hover:after:left-0"
-        >
-          Muistiinpanot
-        </Link>
+          ☰
+        </button>
+
+        {/* Center Section: Desktop Navigation */}
+        <div className="hidden md:flex flex-1 justify-center space-x-6">
+          <Link to="/about" className="text-[#56afe6] hover:text-blue-600">
+            Ohjelmointi
+          </Link>
+          <Link to="/services" className="text-[#56afe6] hover:text-blue-600">
+            Alustat
+          </Link>
+          <Link to="/contact" className="text-[#56afe6] hover:text-blue-600">
+            Forum
+          </Link>
+          <Link to="/contact" className="text-[#56afe6] hover:text-blue-600">
+            Sanakirja
+          </Link>
+          <Link to="/contact" className="text-[#56afe6] hover:text-blue-600">
+            Muistiinpanot
+          </Link>
+        </div>
+
+        {/* Right Section: Login & Signup */}
+        <div className="hidden md:flex space-x-4">
+          <Link to="/login" className="px-4 py-2 bg-[#56afe6] !text-white rounded hover:bg-blue-600 !hover:text-blue-300">
+            Login
+          </Link>
+          <Link to="/signup" className="px-4 py-2 bg-[#56afe6] !text-white rounded hover:bg-blue-600 !hover:text-yellow-300">
+            Sign Up
+          </Link>
+        </div>
       </div>
 
-      {/* Login/Signup */}
-      <div className="space-x-4">
-        <Link
-          to="/login"
-          className="px-4 py-2 bg-[#56afe6] !text-white rounded hover:bg-blue-600 !hover:text-blue-300"
-        >
-          Login
+      {/* Mobile Navigation Menu */}
+      <div className={`md:hidden ${isOpen ? "block" : "hidden"} bg-black bg-opacity-95 p-4 flex flex-col space-y-4 text-center`}>
+        {/* Navigation Links (Stacked) */}
+        <Link to="/about" className="text-[#56afe6] hover:text-blue-600" onClick={() => setIsOpen(false)}>
+          Ohjelmointi
         </Link>
-        <Link
-          to="/signup"
-          className="px-4 py-2 bg-[#56afe6] !text-white rounded hover:bg-blue-600 !hover:!text-yellow-300"
-        >
-          Sign Up
+        <Link to="/services" className="text-[#56afe6] hover:text-blue-600" onClick={() => setIsOpen(false)}>
+          Alustat
         </Link>
+        <Link to="/contact" className="text-[#56afe6] hover:text-blue-600" onClick={() => setIsOpen(false)}>
+          Forum
+        </Link>
+        <Link to="/contact" className="text-[#56afe6] hover:text-blue-600" onClick={() => setIsOpen(false)}>
+          Sanakirja
+        </Link>
+        <Link to="/contact" className="text-[#56afe6] hover:text-blue-600" onClick={() => setIsOpen(false)}>
+          Muistiinpanot
+        </Link>
+
+        {/* Mobile Login/Signup (Stacked) */}
+        <div className="flex flex-col space-y-2">
+          <Link to="/login" className="px-4 py-2 bg-[#56afe6] text-white rounded hover:bg-blue-600 hover:text-blue-300">
+            Login
+          </Link>
+          <Link to="/signup" className="px-4 py-2 bg-[#56afe6] text-white rounded hover:bg-blue-600 hover:text-yellow-300">
+            Sign Up
+          </Link>
+        </div>
       </div>
     </nav>
   );
