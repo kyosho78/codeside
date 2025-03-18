bc0cf9ebb8552c9645f6c076d08ebc6cac4e9d71
Jani Pekkala <pekkala.jani@hotmail.fi>
2025-03-18 13:39:16 +0200

    Forum rakentelua

--- src/Forum/ForumList.jsx
+++ src/Forum/ForumList.jsx
@@ -10,29 +10,29 @@ const ForumList = () => {
     }, []);
 
     return (
-        <div className="bg-blue-900 text-white min-h-screen p-6">
-            <div className="max-w-screen-lg mx-auto">
+        <div className="bg-gray-900 text-white min-h-screen p-6">
+            <div class="relative overflow-x-auto">
                 {/* Aloitusosio */}
                 <div className="text-center mb-10">
-                    <h1 className="text-4xl font-bold text-yellow-400 border-b-4 border-yellow-400 inline-block py-2">Keskustelu Foorumi</h1>
+                <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Keskustelu Foorumi</span></h1>
                 </div>
 
                 {/* Aihealueet */}
-                <div className="bg-blue-800 p-6 rounded-lg shadow-lg">
-                    <h2 className="text-2xl font-semibold text-orange-300 mb-4">Foorumin aihealueet</h2>
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <h2 className="text-2xl font-semibold text-blue-400 mb-4">Foorumin aihealueet</h2>
                     <ul className="space-y-4">
                         {topics.map((topic) => (
-                            <li key={topic.id} className="border-b-2 border-blue-700 py-2">
+                            <li key={topic.id} className="border-b border-gray-700 py-2">
                                 <Link 
                                     to={`/threads/${topic.id}`} 
-                                    className="text-lg text-yellow-300 hover:text-orange-500"
+                                    className="text-lg text-orange-300 hover:text-blue-500"
                                 >
                                     {topic.header}
                                 </Link>
                             </li>
                         ))}
                     </ul>
-                </div>
+                </table>
             </div>
         </div>
     );
