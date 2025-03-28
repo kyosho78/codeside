241e9b395d4f3be03a17ee91801c9c2af267960e
Toni Pekkala <toni.pekkala@hotmail.com>
2025-03-28 18:09:42 +0200

    Lisätty cypress kirjasto. Logintestit ok. Tarkistaa tunnukset, väärät tunnukset ja ilmoitukset oikein.

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,52 @@
+describe("Login Page", () => {
+    it("should render login form", () => {
+      cy.visit("http://localhost:5173/login"); // Muokkaa osoite, jos eri
+      cy.get("input[name='email']").should("exist");
+      cy.get("input[name='password']").should("exist");
+      cy.contains("Kirjaudu sisään").should("exist");
+    });
+
+    it("should log in with valid credentials", () => {
+        cy.visit("http://localhost:5173/login");
+    
+        // Täytetään lomake
+        cy.get("input[name='email']").type("teppo@gmail.com");
+        cy.get("input[name='password']").type("testaaja");
+    
+        // Klikataan Kirjaudu
+        cy.get("button").contains("Kirjaudu").click();
+    
+        // Tarkistetaan että login onnistuu (esim. näkyy ilmoitus tai ohjataan etusivulle)
+        cy.contains("Kirjautuminen onnistui").should("exist");
+
+        // Odotetaan siirtymistä etusivulle
+           cy.url().should("include", "/");
+
+        // Klikataan logout (säädä tämä valitsimen mukaan)
+           cy.contains("Logout").click(); // 
+
+           //Tarkistetaan että käyttäjä ohjataan etusivulle
+           cy.url().should("include", "/");
+    
+        // Vai vaihtoehtoisesti tarkista, että ohjataan etusivulle
+        // cy.url().should("eq", "http://localhost:5173/");
+      });
+
+      it("should show error on invalid credentials", () => {
+        cy.visit("http://localhost:5173/login");
+      
+        cy.get("input[name='email']").type("teppo@gmail.com");
+        cy.get("input[name='password']").type("vääräsalasana");
+      
+        cy.get("button").contains("Kirjaudu").click();
+         // Lisätään pieni viive virheilmoituksen odottamiseen
+
+        cy.wait(500); // odota 1 sekunti
+        // Odotetaan virheilmoitusta
+        cy.contains("Kirjautuminen epäonnistui. Tarkista sähköposti ja salasana.").should("exist");
+      });
+      
+
+
+  });
+  
